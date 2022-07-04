725300a0536ce5950c337107bd4e3787c17f884d
EleniaKRAL <Karo.Rimpler@gmail.com>
2022-07-04 18:35:22 +0200

    Slider for HeatMap

--- assets/js/HeatMap.js
+++ assets/js/HeatMap.js
@@ -104,6 +104,16 @@ var layout = {
 
 var checked = '';
 
+ var mySlider = new rSlider({
+        target: '#sampleSlider',
+        values: [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015],
+        range: true,
+        tooltip: true,
+        scale: true,
+        labels: true,
+        set: [2010, 2013]
+    });
+
 //add a checked-effect to the gradient buttons by changing css style
 function colorchange(b) {
 
