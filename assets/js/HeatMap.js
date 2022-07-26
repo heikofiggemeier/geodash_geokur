c57bcfba97cfdb09c134dbdab4fb38db37bca3f5
EleniaKRAL <Karo.Rimpler@gmail.com>
2022-07-26 19:47:01 +0200

    testing new slider
    to update heatmap, uncheck and check again the dataset checkboxes

--- assets/js/HeatMap.js
+++ assets/js/HeatMap.js
@@ -1,7 +1,8 @@
-var values1a = [30];
-var value1a = 0;
-var values1b = [70];
-var value1b = 0;
+var slider1 = document.getElementById("range-a");
+var slider2 = document.getElementById("range-b");
+var slidertrack = document.getElementById("slider-track");
+var display1 = document.getElementById("range1");
+var display2 = document.getElementById("range2");
 
 
 var xValues = []; //will get Quality Metrix labels
@@ -13,22 +14,28 @@ var zValues = []; //will get Dataset values
 
 
 var z5 = []; //Dataset array for Dataset A
-var z5om = 7;
-var z5com = 78;
-var z5qaa = 95;
-var z5tcc = 99;
+var z5fin = []
+var z5om = 41;
+var z5com = 64;
+var z5qaa = 5;
+var z5tcc = 88;
 
 var z6 = []; //Dataset array for Dataset B
+var z6fin = []
 var z6om = 68;
 var z6com = 51;
 var z6qaa = 70;
 var z6tcc = 36;
 
 var z7 = []; //Dataset array for Dataset C
+var z7fin = []
 var z7om = 92;
 var z7com = 5;
 var z7qaa = 45;
 var z7tcc = 78;
+
+
+
 
 // predefine color gradients to choose from
 var colorscaleValue = [
@@ -110,12 +117,15 @@ var layout = {
 	}
 };
 
+
+
 var checked = '';
 
 //add a checked-effect to the gradient buttons by changing css style
 function colorchange(b) {
 
 	if (b.className == "btn-hm btn-1") {
+		slidertrack.style.backgroundImage = 'linear-gradient(to right, #c9461c 0%, #e86b21 30%, #f7bb32 60%, #6ba316 100%) ';
 
 		if (checked.style != undefined) {
 
@@ -137,6 +147,7 @@ function colorchange(b) {
 		}
 	}
 	if (b.className == "btn-hm btn-2") {
+		slidertrack.style.backgroundImage = 'linear-gradient(to right, #ffffcc 0%, #c7e9b4 30%, #41b6c4 60%, #1d91c0 100%) ';
 
 		if (checked.style != undefined) {
 
@@ -158,6 +169,7 @@ function colorchange(b) {
 		}
 	}
 	if (b.className == "btn-hm btn-3") {
+		slidertrack.style.backgroundImage = 'linear-gradient(to right, #f7f7f7 0%, #bdbdbd 30%, #969696 60%, #737373 100%) ';
 
 		if (checked.style != undefined) {
 
@@ -182,45 +194,14 @@ function colorchange(b) {
 }
 
 
-// get slider values
-function rangevalue() {
-
-	values1a.push(document.getElementById("range-1a").value);
-	values1b.push(document.getElementById("range-1b").value);
-
-
-	if (values1a.length > 2) {
-		values1a.splice(0, 1);
-
-	}
-	if (values1a[values1a.length - 1] != values1a[values1a.length - 2]) {
-		value1a = values1a[values1a.length - 1];
-		console.log(value1a);
-
-
-	}
-	if (values1b.length > 2) {
-		values1b.splice(0, 1);
-
-	}
-	if (values1b[values1b.length - 1] != values1b[values1b.length - 2]) {
-		value1b = values1b[values1b.length - 1];
-		console.log(value1b);
-	}
-
-return value1a;
-}
-
-
-
-
 //add data to the Dataset arrays and x-axis labels depending on selected Quality Metric buttons
+
 function chooseQ(c) {
-	
 	if (document.getElementById("om").checked &&
 		(z5.includes(z5om) == false && z6.includes(z6om) == false && z7.includes(z7om) == false)) {
 
 		layout.annotations = [];
+
 		z5.push(z5om);
 		z6.push(z6om);
 		z7.push(z7om);
@@ -338,50 +319,110 @@ function chooseQ(c) {
 	Plotly.newPlot('myDiv', data, layout);
 }
 
+// get slider values
+function rangevalue1() {
+
+	/*if (slider2.value < slider1.value) {
+		slider1.value = parseInt(slider2.value);
+	}*/
+	display1.textContent = slider1.value;
+	chooseQ();
+
+}
+
+function rangevalue2() {
+
+	/*if (slider2.value < slider1.value) {
+		slider2.value = parseInt(slider1.value);
+	}*/
+	display2.textContent = slider2.value;
+	
+}
+
+function slider(){
+	//ToDo
+}
+
 //add Dataset arrays and y-axis labels to the chart depending on checked Dataset buttons
 function chooseD(e) {
 
-	if (document.getElementById("l5").checked && zValues.includes(z5) == false) {
+	if (document.getElementById("l5").checked && zValues.includes(z5fin) == false) {
+
+		for (var i = 0; i < z5.length; i++) {
+
+			if (z5[i] >= slider1.value && z5[i] <= slider2.value) {
+				z5fin.push(z5[i]);
+
+			} else {
+				z5fin.push(null);
+			}
+		}
 
 		layout.annotations = [];
 		yValues.push('Dataset A');
-		zValues.push(z5);
+		zValues.push(z5fin);
 
-	} else if (document.getElementById("l5").checked == false && zValues.includes(z5) == true) {
+	} else if (document.getElementById("l5").checked == false && zValues.includes(z5fin) == true) {
 
 		layout.annotations = [];
 		var del1 = yValues.indexOf('Dataset A');
-		var del2 = zValues.indexOf(z5);
+		var del2 = zValues.indexOf(z5fin);
 		yValues.splice(del1, 1);
 		zValues.splice(del2, 1);
+		z5fin = [];
+		
 
-	} else if (document.getElementById("l6").checked && zValues.includes(z6) == false) {
+	}if (document.getElementById("l6").checked && zValues.includes(z6fin) == false) {
+
+		for (var i = 0; i < z6.length; i++) {
+
+			if (z6[i] >= slider1.value && z6[i] <= slider2.value) {
+				z6fin.push(z6[i]);
+
+			} else {
+				z6fin.push(null);
+			}
+		}
 
 		layout.annotations = [];
 		yValues.push('Dataset B');
-		zValues.push(z6);
+		zValues.push(z6fin);
 
-	} else if (document.getElementById("l6").checked == false && zValues.includes(z6) == true) {
+	} else if (document.getElementById("l6").checked == false && zValues.includes(z6fin) == true) {
 
 		layout.annotations = [];
 		var del1 = yValues.indexOf('Dataset B');
-		var del2 = zValues.indexOf(z6);
+		var del2 = zValues.indexOf(z6fin);
 		yValues.splice(del1, 1);
 		zValues.splice(del2, 1);
+		z6fin = [];
+		
 
-	} else if (document.getElementById("l7").checked && zValues.includes(z7) == false) {
+	}if (document.getElementById("l7").checked && zValues.includes(z7fin) == false) {
+
+		for (var i = 0; i < z7.length; i++) {
+
+			if (z7[i] >= slider1.value && z7[i] <= slider2.value) {
+				z7fin.push(z7[i]);
+
+			} else {
+				z7fin.push(null);
+			}
+		}
 
 		layout.annotations = [];
 		yValues.push('Dataset C');
-		zValues.push(z7);
+		zValues.push(z7fin);
 
-	} else if (document.getElementById("l7").checked == false && zValues.includes(z7) == true) {
+	} else if (document.getElementById("l7").checked == false && zValues.includes(z7fin) == true) {
 
 		layout.annotations = [];
 		var del1 = yValues.indexOf('Dataset C');
-		var del2 = zValues.indexOf(z7);
+		var del2 = zValues.indexOf(z7fin);
 		yValues.splice(del1, 1);
 		zValues.splice(del2, 1);
+		z7fin = [];
+		
 
 	}
 
